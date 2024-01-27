ecd06aed8d0c3ab001217c5c7d3907d3a5499a63
venom <abdo30201080101694@gmail.com>
2024-01-27 16:35:36 +0200

    finish part-1

---
+++ Post-3/index.js
@@ -0,0 +1,8 @@
+// console.log(`Hello from console`);
+// console.log(`my name is abdelrahman`, `my age is`, 22);
+
+// console.debug(`here is my name`);
+
+// console.info("This is an informational message.");
+
+console.warn("This is a warning message.");
